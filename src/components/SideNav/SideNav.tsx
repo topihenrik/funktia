56932932f86d5607484d699ff6a626ffa886be63
Topi Ilmoni <topi.ilmoni@gmail.com>
2024-11-16 14:48:41 +0200

    Fixed Tabs and SideNav

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -2,6 +2,7 @@ import { icons } from "lucide-react";
 import { Heading } from "../Heading";
 import { ClickElement } from "../ClickElement";
 import { tv } from "tailwind-variants";
+import { useMemo } from "react";
 
 const containerStyle = tv({
     base: ["flex flex-col gap-8 border-r-2 w-64 p-4"]
@@ -14,20 +15,20 @@ const ulStyle = tv({
 const clickElementStyle = tv({
     base: ["rounded", "hover:bg-blue-100 focus:outline-blue-900"],
     variants: {
-        active: {
-            true: {
-                clickElement: ["bg-blue-100 text-blue-600"]
-            }
+        isActive: {
+            true: "bg-blue-100 text-blue-600"
         }
     }
 });
 
-export interface SideNavItem {
+interface BaseSideNavItem {
     text: string;
     icon: keyof typeof icons;
     route: string;
-    isActive?: boolean;
+    isActive: boolean;
 }
+
+export type SideNavItem = Omit<BaseSideNavItem, "isActive">;
 
 interface SideNavProps {
     /**
@@ -38,21 +39,34 @@ interface SideNavProps {
      * Side navigation items
      */
     items: SideNavItem[];
+    /**
+     * Callback for navigation
+     */
+    navigationCallback: (to: string) => void;
 }
 
-export function SideNav({ title, items }: SideNavProps) {
+export function SideNav({ title, navigationCallback, items }: SideNavProps) {
+    const sidNavItems: BaseSideNavItem[] = useMemo(
+        () =>
+            items.map((item) => ({
+                ...item,
+                isActive: window.location.pathname.includes(item.route)
+            })),
+        [items, window.location.pathname]
+    );
+
     return (
         <div className={containerStyle()}>
             <Heading level={2}>{title}</Heading>
             <nav>
                 <ul className={ulStyle()}>
-                    {items.map((item, i) => (
+                    {sidNavItems.map((item, i) => (
                         <li key={`sn${i}`}>
                             <ClickElement
-                                className={`${clickElementStyle({ active: item.isActive })}`}
+                                className={clickElementStyle({ isActive: item.isActive })}
                                 variant="link"
+                                onPress={() => navigationCallback(item.route)}
                                 icon={item.icon}
-                                href={item.route}
                             >
                                 {item.text}
                             </ClickElement>
